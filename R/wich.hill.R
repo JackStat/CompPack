62fe295dd48688278a01c0dd476ef029fab41106
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-09 15:31:15 -0600

    fixing seed

--- R/wich.hill.R
+++ R/wich.hill.R
@@ -12,9 +12,9 @@ function(){
 	}
 		
 
-	.wh.seed[1] <<- 171 * (.wh.seed[1] %% 30269) 
-	.wh.seed[2] <<- 172 * (.wh.seed[2] %% 30307) 
-	.wh.seed[3] <<- 170 * (.wh.seed[3] %% 30323)
+	.wh.seed[1]<<-171 * (.wh.seed[1] %% 30269) 
+	.wh.seed[2]<<-172 * (.wh.seed[2] %% 30307) 
+	.wh.seed[3]<<-170 * (.wh.seed[3] %% 30323)
 	
 	x<- (.wh.seed[1] / 30269) + (.wh.seed[2] / 30307) + (.wh.seed[3] / 30323)
 	x - floor(x)
