3578ae9a9af6393248c074c047f5632eacb7d269
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-10 17:28:13 -0600

    adding descriptions

--- R/wich.hill.R
+++ R/wich.hill.R
@@ -1,14 +1,24 @@
 #' Wichman-Hill Random Number Generator
 #' 
+#' @description hi
 #' 
 #' @author Tyler Hunt \email{tyler@@psychoanalytix.com} 
+#' 
+#' @references
+#'  Wichmann, B. A., & Hill, I. D. (1982). Algorithm AS 183: An efficient and portable pseudo-random number generator. Journal of the Royal Statistical Society. Series C (Applied Statistics), 31(2), 188-190.
+#'  
+#'  Ripley, B. D. (2009). Stochastic simulation (Vol. 316). Wiley.
+#'  
+#'  @examples
+#'  replicate(10,wich.hill())
+#'  
 #' @export
 
 wich.hill <-
 function(){
 
 	if(!exists(".wh.seed")){ 
-    .wh.seed<-c(1,1,1)
+    .wh.seed<<-c(1,1,1)
 	}
 		
 	.wh.seed[1]<<-171 * (.wh.seed[1] %% 30269) 
