f5795ba710fc4355c11ea4e093caaee7463cc76e
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-10 11:00:46 -0600

    tweaking seed

--- R/wich.hill.R
+++ R/wich.hill.R
@@ -7,9 +7,9 @@
 wich.hill <-
 function(){
 
-	if(sum(ls(all.names=TRUE)==".wh.seed")!=0) 
+	if(sum(ls(all.names=TRUE)==".wh.seed")!=0){ 
     .wh.seed<-c(1,1,1)
-
+	}
 		
 	.wh.seed[1]<<-171 * (.wh.seed[1] %% 30269) 
 	.wh.seed[2]<<-172 * (.wh.seed[2] %% 30307) 
