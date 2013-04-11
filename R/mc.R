5b96adaa57df4d6c946263b526411737404107bf
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-11 11:21:02 -0600

    changing structure of example in mc

--- R/mc.R
+++ R/mc.R
@@ -7,6 +7,7 @@
 #' @author Tyler Hunt \email{tyler@@psychoanalytix.com}
 #' 
 #' @examples
+#' 
 #'  A<-matrix(c(.9,.8,0,0,.1,.2,0,0,0,0,.5,.6,0,0,.5,.4), nrow=4)
 #'  B<-matrix(rep(.3,16), nrow=4); diag(B)<-.1
 #'  C<-matrix(c(.9,0,0,0, .1,.9,0,0 ,0,.1,.8,.1, 0,0,.2,.9), nrow=4)
