482e93867fa353cabdc85233e0de691e9559e330
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-11 11:29:21 -0600

    Added in multivariate normal random generator

---
+++ R/mvrand.norm.R
@@ -0,0 +1,20 @@
+#' Generate Multivariate Normal Distribution
+#' 
+#' @description This method for generating random multivariate normal data is based on a cholesky decomposition.
+#' @param n the number of observations
+#' @param mu a vector of means
+#' @param sigma a variance-covariance matrix
+#' 
+#' @author Tyler Hunt \email{tyler@@psychoanalytix.com}
+#' 
+#' @export
+
+mvrand.norm<-function(n, mu, sigma){
+  k<-length(mu)
+  vs<-matrix(rep(NA, k*n), ncol=k)
+  for(i in 1:n){
+    vs[i,]<-rnorm(k,mu,1)
+    vs[i,]<-t(chol(sigma))%*%vs[i,]
+  }
+  vs
+}
