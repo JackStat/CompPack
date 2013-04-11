89bf29e6eee5414bb4f358fc1a52d4c67bc63a39
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-10 22:34:57 -0600

    Adding in newton raphson

---
+++ R/newton.raph.R
@@ -0,0 +1,25 @@
+#' Newton Raphson Alogorithm
+#' 
+#' @param f The function that you want to optimize.
+#' @param df the 1st derivative of the function.
+#' @param d2f the 2nd derivative of the function.
+#' @param start The starting value for optimization.
+#' @param tol The level of tolerance desired for convergence.
+#' 
+#' 
+#' @author Tyler Hunt \email{tyler@@psychoanalytix.com}
+#' @export
+
+
+newton.raph <- function(f, df, d2f, start, tol=0.000001){
+  
+  new <- start + 10*tol
+  iter <- 0
+  while(abs(start - new) > tol){
+    iter <- iter+1
+    new <- start
+    start <- start - df(start)/d2f(start)
+  }
+  result<-c(Estimate=start, Iterations=iter, Likelihood=f(start))
+  return(result)
+}
