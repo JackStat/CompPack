ea8aba04aa3ea3b49729b6b9db9ca91591d0769d
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-11 10:55:28 -0600

    Adding in markov plotter

---
+++ R/plot.mc.R
@@ -0,0 +1,20 @@
+#' Plot Markov Chains
+#' 
+#' @param reps the number of replications
+#' @param prob the probability matrix
+#' 
+#' @author Tyler Hunt \email{tyler@@psychoanalytix.com}
+#' 
+#' @examples
+#'  A<-matrix(c(.9,.8,0,0,.1,.2,0,0,0,0,.5,.6,0,0,.5,.4), nrow=4)
+#'  B<-matrix(rep(.3,16), nrow=4); diag(B)<-.1
+#'  C<-matrix(c(.9,0,0,0, .1,.9,0,0 ,0,.1,.8,.1, 0,0,.2,.9), nrow=4)
+#'  D<-matrix(c(.9,0,0,.1,.1,.9,0,0,0,.1,.9,0,0,0,.1,.9), nrow=4)
+#' plot.mc(100, B)
+#' 
+#' 
+#' @export
+
+plot.mc(reps, prob){
+  plot(1:reps, markov.chain(prob, reps), "l")
+}
