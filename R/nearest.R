44c2bba85d5b5e1793354fbb3a2b7d859c491c6a
Tyler Hunt <tyler.hunt@utah.edu>
2013-04-13 10:35:29 -0600

    Updated documentation

--- R/nearest.R
+++ R/nearest.R
@@ -2,7 +2,7 @@
 #' 
 #' @description Find the location of the nearest value to a number that you specify.
 #' 
-#' @param x a vector or matrix.
+#' @param x a vector, matrix, or data frame.
 #' @param value the value that you want to find.
 #' 
 #' 
